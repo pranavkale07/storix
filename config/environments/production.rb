b740279adfb33c11c46db15a0f1a31c1a9ba8fab
pranavkale07 <pranavkale911@gmail.com>
2025-07-22 20:24:28 +0000

    add queue migrations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -91,7 +90,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
+    "storix.prxnav.dev"     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   #
